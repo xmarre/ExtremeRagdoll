41c770988e40e4a299dff948672d9cb6d21abe38
xmarre <54859656+xmarre@users.noreply.github.com>
2025-09-26 06:52:58 +0200

    Make death shove radial and dedupe re-entries

--- ExtremeRagdoll/SubModule.cs
+++ ExtremeRagdoll/SubModule.cs
@@ -5,14 +5,41 @@ namespace ExtremeRagdoll
 {
     public class SubModule : MBSubModuleBase
     {
+        private static bool _adapted;
+
         protected override void OnSubModuleLoad()
         {
             new Harmony("extremeragdoll.patch").PatchAll();
         }
 
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            if (_adapted) return;
+            try
+            {
+                _adapted = ER_TOR_Adapter.TryEnableShockwaves();
+            }
+            catch
+            {
+                _adapted = false;
+            }
+        }
+
         public override void OnMissionBehaviorInitialize(Mission mission)
         {
-            mission.AddMissionBehavior(new ER_DeathBlastBehavior());
+            if (!_adapted)
+            {
+                try
+                {
+                    _adapted = ER_TOR_Adapter.TryEnableShockwaves();
+                }
+                catch
+                {
+                    _adapted = false;
+                }
+            }
+
+            mission.AddMissionBehavior(new ER_DeathBlastBehavior()); // optional fallback
         }
     }
 }
