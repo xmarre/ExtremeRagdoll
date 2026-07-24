a068e637d1cb5a50d09ce012f46381beea969623
xmarre <54859656+xmarre@users.noreply.github.com>
2026-07-24 06:15:33 +0200

    Add isolated ClothSync test submodule

---
+++ ClothSyncTests/SubModule.cs
@@ -0,0 +1,24 @@
+using TaleWorlds.MountAndBlade;
+
+namespace ExtremeRagdoll.ClothSyncTests
+{
+    public sealed class SubModule : MBSubModuleBase
+    {
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            try
+            {
+                _ = Settings.Instance;
+            }
+            catch
+            {
+                // MCM is an existing required dependency of ExtremeRagdoll.
+            }
+        }
+
+        public override void OnMissionBehaviorInitialize(Mission mission)
+        {
+            mission.AddMissionBehavior(new ClothSyncTestBehavior());
+        }
+    }
+}
