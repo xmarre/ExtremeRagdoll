697fc08de8af081f4c5e24015f0447d19d3e3792
xmarre <54859656+xmarre@users.noreply.github.com>
2026-07-24 19:20:39 +0200

    Register cloth-safe velocity governor behavior

--- ClothSyncTests/SubModule.cs
+++ ClothSyncTests/SubModule.cs
@@ -21,6 +21,7 @@ namespace ExtremeRagdoll.ClothSyncTests
             mission.AddMissionBehavior(new ClothSyncTestBehavior());
             mission.AddMissionBehavior(new AgentClothOwnerTestBehavior());
             mission.AddMissionBehavior(new VisualTickCatchUpBehavior());
+            mission.AddMissionBehavior(new ClothSafeVelocityGovernorBehavior());
         }
     }
 }
