4b783a7de4fd07b5cc0a14d5a2d9375ce016abf0
xmarre <54859656+xmarre@users.noreply.github.com>
2026-07-26 00:46:28 +0200

    Update v1.1.13 build hashes and compile hotfix

--- tools/ga1113/reconstruct-payloads.ps1
+++ tools/ga1113/reconstruct-payloads.ps1
@@ -29,9 +29,9 @@ $patch=@(
  @{Path=(Join-Path $root 'patchchunks/p02.b64');Hash='19ecfcfa5ddc2ad76595b6feb1b2c76eb350e76acc6af78494a529edb8fbca75'}
 )
 $test=@(@{Path=(Join-Path $root 'testchunks/p00.b64');Hash='6751ab4acf8caa861fc66e9d17993dda8e9c998288b82bfebe253feb49634a34'})
-$build=@(@{Path=(Join-Path $root 'buildchunks/p00.b64');Hash='6f203284f4947192aedcaeed091025512ece5f39c3b5887b27a30e01377150ae'})
+$build=@(@{Path=(Join-Path $root 'buildchunks/p00.b64');Hash='38e501ac5ebb28332cd9693ce3c6bfff69d6145d1817a8bab23931efdd8a02eb'})
 Join-Chunks $patch (Join-Path $root 'patch.gz.b64') '197522f6418b2f27bf7ef048bd4948362c2cfe5f3434c286b8ad681322f24b2a'
 Join-Chunks $test (Join-Path $root 'test.gz.b64') '6751ab4acf8caa861fc66e9d17993dda8e9c998288b82bfebe253feb49634a34'
-Join-Chunks $build (Join-Path $root 'build.gz.b64') '6f203284f4947192aedcaeed091025512ece5f39c3b5887b27a30e01377150ae'
-Expand-GzipBase64 (Join-Path $root 'build.gz.b64') (Join-Path $root 'build-ga1113-camera-split-crash.ps1') 'ac94227ce84c485797ad3f5dbba21d352e1ef9903343ea315d844b3ffeff10c5' '8942e1cb60b905cf9a5c568bbe6f815b081498ed361d719b5e3fa06a647a26ca'
+Join-Chunks $build (Join-Path $root 'build.gz.b64') '38e501ac5ebb28332cd9693ce3c6bfff69d6145d1817a8bab23931efdd8a02eb'
+Expand-GzipBase64 (Join-Path $root 'build.gz.b64') (Join-Path $root 'build-ga1113-camera-split-crash.ps1') '9a2be50339ffad203974503394e378e2a934c07b750d16b644893ddd0c0e208d' '12d0e9afa4a400117819a34671f6a2313ab54b8fa1babf4f1665e44e9f074549'
 Write-Host 'GA1113_PAYLOAD_RECONSTRUCTION=SUCCESS'
